2c4fcc91a08530967bef0a1625b47f4179565950
LimDexter-RP <83123867+LimDexter-RP@users.noreply.github.com>
2021-08-04 13:36:45 +0800

    Updated SubjectMain.java
    Added sysout messages
    Added check for empty list for deletion

--- W67M_T2_CPA/src/SubjectMain.java
+++ W67M_T2_CPA/src/SubjectMain.java
@@ -13,10 +13,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 
 public class SubjectMain {
-
+private static final AtomicInteger count = new AtomicInteger(0);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		private static final AtomicInteger count = new AtomicInteger(0);
+		
 			
 			// TODO Auto-generated method stub
 			ArrayList<Subject> subjectList = new ArrayList<Subject>();
@@ -59,10 +59,11 @@ public class SubjectMain {
 			System.out.println("4. Quit The Program");
 		}
 		//View All Subject For Option 1
-		public static void viewAllSubjects(ArrayList<Subject1> subjectList) {
+		public static void viewAllSubjects(ArrayList<Subject> subjectList) {
 			Helper.line(40, "-");
 			System.out.println("VIEW ALL SUBJECTS");
 			Helper.line(40, "-");
+				System.out.printf("%-10s %-10s\n", "ID", "SUBJECT");
 			for (int i = 0; i < subjectList.size(); i++)
 				System.out.printf("%-10d %-10s\n", subjectList.get(i).getSubjectID(), subjectList.get(i).getSubjectName());
 		}
@@ -73,28 +74,33 @@ public class SubjectMain {
 			boolean count = false;
 			for (int i = 0; i < subjectList.size(); i ++) {
 				if (subjectList.get(i).getSubjectName() == subject ) {
-					System.out.println("This Subject Has Been Added!");
+					System.out.println("Subject Already Exists!");
 					count = true;
 				}
 			}
 			if (count == false) {
-				Subject1 addSubject = new Subject1(id, subject);
+				Subject addSubject = new Subject(id, subject);
 				subjectList.add(0, addSubject);
+				System.out.println(subject + " Has Been Added!");
 			}
 		}
 		//Delete A Subject For Option 3
-		public static boolean deleteSubject(ArrayList<Subject1> subjectList) {
+		public static boolean deleteSubject(ArrayList<Subject> subjectList) {
 			String subject = Helper.readString("Enter Subject Name To Delete > ");
 			
 			for(int i = 0; i < subjectList.size(); i ++) {
 				if(!subjectList.isEmpty() && subjectList.get(i).getSubjectName().equalsIgnoreCase(subject)) {
 					subjectList.remove(i);
+					System.out.println(subject + " Has Been Deleted From The List!");
 					return true;
 				}
+			}if (subjectList.isEmpty()) {
+			System.out.println("The List Is Empty!");
+			return false;
 			}
 			return false;
 			
 		}
 	}
 
-}
+
