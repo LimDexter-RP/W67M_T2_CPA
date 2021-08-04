1055aec13852278baef9f303e7e8303ad23e9fd9
LimDexter-RP <20039529@myrp.edu.sg>
2021-08-04 12:53:23 +0800

    Inital Commit

---
+++ W67M_T2_CPA/src/Subject.java
@@ -0,0 +1,43 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Lim Dexter
+ * Student ID: 20039529
+ * Class: C208
+ * Date/Time created: Wednesday 04-08-2021 12:49
+ */
+
+public class Subject {
+
+	private int subjectID;
+	private String subjectName;
+
+	public Subject(int subjectID, String subjectName) {
+		this.subjectID = subjectID;
+		this.subjectName = subjectName;
+	}
+
+	public int getSubjectID() {
+		return subjectID;
+	}
+
+	public void setSubjectID(int subjectID) {
+		this.subjectID = subjectID;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+
+	}
+
+
+
+
---
+++ W67M_T2_CPA/src/SubjectMain.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Lim Dexter
+ * Student ID: 20039529
+ * Class: C208
+ * Date/Time created: Wednesday 04-08-2021 12:49
+ */
+
+public class SubjectMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private static final AtomicInteger count = new AtomicInteger(0);
+			
+			// TODO Auto-generated method stub
+			ArrayList<Subject> subjectList = new ArrayList<Subject>();
+
+			int option = -1;
+			{
+
+				while (option != 4) {
+					SubjectMain.menu();
+					option = Helper.readInt("Enter Option > ");
+
+					if (option == 1) {
+
+						SubjectMain.viewAllSubjects(subjectList);
+
+					} else if (option == 2) {
+						SubjectMain.addSubjects(subjectList);
+
+					} else if (option == 3) {
+						SubjectMain.deleteSubject(subjectList);
+
+					} else {
+
+						System.out.println("Good Bye!");
+
+					}
+				}
+			}
+
+		}
+
+		//Menu method for printing later
+		public static void menu() {
+			Helper.line(30, "=");
+			System.out.println("SUBJECT MANAGEMENT");
+			Helper.line(30, "=");
+			System.out.println("1. View All Subjects");
+			System.out.println("2. Add A Subject");
+			System.out.println("3. Delete A Subject");
+			System.out.println("4. Quit The Program");
+		}
+		//View All Subject For Option 1
+		public static void viewAllSubjects(ArrayList<Subject1> subjectList) {
+			Helper.line(40, "-");
+			System.out.println("VIEW ALL SUBJECTS");
+			Helper.line(40, "-");
+			for (int i = 0; i < subjectList.size(); i++)
+				System.out.printf("%-10d %-10s\n", subjectList.get(i).getSubjectID(), subjectList.get(i).getSubjectName());
+		}
+		//Add Subject For Option 2
+		public static void addSubjects(ArrayList<Subject> subjectList) {
+			int id = count.incrementAndGet();
+			String subject = Helper.readString("Enter Subject To Add > ");
+			boolean count = false;
+			for (int i = 0; i < subjectList.size(); i ++) {
+				if (subjectList.get(i).getSubjectName() == subject ) {
+					System.out.println("This Subject Has Been Added!");
+					count = true;
+				}
+			}
+			if (count == false) {
+				Subject1 addSubject = new Subject1(id, subject);
+				subjectList.add(0, addSubject);
+			}
+		}
+		//Delete A Subject For Option 3
+		public static boolean deleteSubject(ArrayList<Subject1> subjectList) {
+			String subject = Helper.readString("Enter Subject Name To Delete > ");
+			
+			for(int i = 0; i < subjectList.size(); i ++) {
+				if(!subjectList.isEmpty() && subjectList.get(i).getSubjectName().equalsIgnoreCase(subject)) {
+					subjectList.remove(i);
+					return true;
+				}
+			}
+			return false;
+			
+		}
+	}
+
+}
